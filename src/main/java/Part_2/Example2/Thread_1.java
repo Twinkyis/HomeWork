c01906d1a8b4eb45b3865f7b976113d0dfab4341
ROG 2022 <vvv1370@gmail.com>
2022-11-07 19:56:11 +0200

    new from 07.11.2022 v1.0

--- src/main/java/Part_2/Example2/Thread_1.java
+++ src/main/java/Part_2/Example2/Thread_1.java
@@ -1,27 +1,21 @@
 package Part_2.Example2;
 
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 
-public class Thread_1 extends Thread{
-   public static Integer c = null;
+public class Thread_1 implements Callable {
 
     @Override
-    public void run() {
+    public Object call() {
 
         ArrayList1 arrayList1 = new ArrayList1();
-
-        for (String s : arrayList1.list2) {}
 
         Scanner in = new Scanner(System.in);
         System.out.print("Input a number: ");
         c = in.nextInt();
+        in.close();
 
-       try {
-           Thread.sleep(5000);
-           System.out.print(arrayList1.list2.get(c));
-           in.close();
-       } catch (InterruptedException e) {
-           throw new RuntimeException(e);
-       }
+        return arrayList1.list2.get(c);
     }
+   public static Integer c = null;
 }
--- src/main/java/Part_2/Example2/main1.java
+++ src/main/java/Part_2/Example2/main1.java
@@ -6,20 +6,23 @@ import Part_2.Example2.Thread_2;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 import static java.lang.System.in;
 
 public class main1 {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 
-        Thread_1 thread_1 = new Thread_1();
+
+        Callable<String> thread_1 = new Thread_1();
+        FutureTask futureTask = new FutureTask(thread_1);
         Thread_2 thread_2 = new Thread_2();
         thread_2.setDaemon(true);
 
-        thread_1.start();
+        new Thread(futureTask).start();
         thread_2.start();
-        thread_1.join();
         thread_2.join();
-
+        System.out.print(futureTask.get());
     }
 }
